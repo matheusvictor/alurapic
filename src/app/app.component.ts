afd5ba738b1e2767a1b80a6b32c6cd5a7aa86d3f
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-27 21:16:58 -0300

    Aplica data biding ao template do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Game Station';
+  url = 'https://images.unsplash.com/photo-1511882150382-421056c89033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80';
 }
