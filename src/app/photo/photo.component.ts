533186b533c5479f04cef07e6a7afc77f56863d9
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-27 21:40:26 -0300

    Cria primeiro componente
    O componente criado também foi declarado no módulo da aplicação

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent  {
+
+  description = 'Game Station';
+  url = 'https://images.unsplash.com/photo-1511882150382-421056c89033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80';
+
+}
