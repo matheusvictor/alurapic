015cd02a7b9a64c22ae7747be808c96932c185b9
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-27 22:55:09 -0300

    Importa módulo HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,15 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
-    {
-      url: "https://images.unsplash.com/photo-1511882150382-421056c89033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-      description: "Game Station"
-    },
-    {
-      url: "https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      description: "Gamer"
-    }
-  ];
+  photos = [];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 
 }
