eb6cbaf75a7b8e3aa2c3f7ff8a2d2b4a1d1287ec
Matheus Victor <matheusvictor.salles@gmail.com>
2022-03-01 20:10:07 -0300

    Cria novos módulos e add roteamentos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,21 @@
-import { HttpClient } from '@angular/common/http';
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent],
-  imports: [HttpClient]
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent
+  ],
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ]
 })
 export class PhotosModule {
 
